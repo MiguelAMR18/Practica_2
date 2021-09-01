34391d5c9511260f8b772c3dbb447378a08aa512
a1138143@uabc.edu.mx <a1138143@uabc.edu.mx>
2021-08-31 19:50:58 -0700

    tarea

---
+++ Ejercicio_StringInt/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_StringInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Edad; 
+            string Nombre;
+            string Edada;
+
+            Console.WriteLine("Escribe tu Nombre:  ");
+            Nombre = Console.ReadLine();
+            Console.WriteLine("que edad tienes: ");
+            Edada = Console.ReadLine();
+            Edad = Convert.ToInt32(Edada); 
+            Console.WriteLine("Te llamas " + Nombre + " y tienes: " + Edad + " años");
+            Console.WriteLine("preciona cualquier tecla para continuar");
+            Console.ReadKey();
+        }
+    }
+}
