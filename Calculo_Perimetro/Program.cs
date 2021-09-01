4e8de9c966066f3f7c46ec3028b3472d294120e5
a1138143@uabc.edu.mx <a1138143@uabc.edu.mx>
2021-08-31 19:16:14 -0700

    Agregar archivos de proyecto.

---
+++ Calculo_Perimetro/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculo_Perimetro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int perimetro_a = 10;
+            int perimetro_b = 10;
+            int perimetro_c = 7;
+            int peri = perimetro_a + perimetro_b + perimetro_c;
+
+            Console.WriteLine("El perimetro del triangulo es: " + peri);
+            Console.WriteLine("preciona cualquier tecla para continuar");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio_String/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string Nombre, Ciudad;
+            Console.WriteLine("Escribe tu Nombre:  ");
+            Nombre = Console.ReadLine();
+            Console.WriteLine("Escribe una Ciudad: ");
+            Ciudad = Console.ReadLine();
+            Console.WriteLine("Hola " + Nombre + " Bienbenido a " + Ciudad);
+            Console.WriteLine("preciona cualquier tecla para continuar");
+            Console.ReadKey();
+        }
+    }
+}
